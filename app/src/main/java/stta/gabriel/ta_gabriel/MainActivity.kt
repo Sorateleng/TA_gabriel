3e1bcf8a4d319c93fc73115d9d469887aeee4ab3
adeds <adeyds@gmail.com>
2020-02-12 21:08:46 +0700

    fixing login layout

--- app/src/main/java/stta/gabriel/ta_gabriel/MainActivity.kt
+++ app/src/main/java/stta/gabriel/ta_gabriel/MainActivity.kt
@@ -1,12 +1,31 @@
 package stta.gabriel.ta_gabriel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        requestWindowFeature(Window.FEATURE_ACTION_BAR)
         setContentView(R.layout.activity_main)
+
+        btnlogin.setOnClickListener {
+            val id = inputId.text.toString()
+            val password = inputPassword.text.toString()
+            if (id.trim().isNullOrBlank())
+                inputId.error = "Masukkan ID Anda terlebih dahulu"
+            else if (password.trim().isNullOrBlank())
+                inputPassword.error = "Masukkan Password Anda terlebih dahulu"
+            else Toast.makeText(
+                this,
+                "ID Anda : $id \nPassword Anda : $password",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
