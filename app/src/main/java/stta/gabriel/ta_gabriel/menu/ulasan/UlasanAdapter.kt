1afcbcce12d8cb72134b687fe30eb9a9e6d4a6ed
adesamp <ade@sampingan.co.id>
2020-05-01 04:41:26 +0700

    fixing ulasan adapter

--- app/src/main/java/stta/gabriel/ta_gabriel/menu/ulasan/UlasanAdapter.kt
+++ app/src/main/java/stta/gabriel/ta_gabriel/menu/ulasan/UlasanAdapter.kt
@@ -1,5 +1,51 @@
 package stta.gabriel.ta_gabriel.menu.ulasan
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_laporan.view.*
+import stta.gabriel.ta_gabriel.R
+import stta.gabriel.ta_gabriel.model.ItemUlasan
 
-class UlasanAdapter {
+
+class UlasanAdapter(
+    private val items: MutableList<ItemUlasan>,
+    private val callback: ItemAdapterCallback
+) : RecyclerView.Adapter<UlasanAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.item_riwayat,parent, false)
+        return Holder(itemView = view)
+    }
+
+    override fun getItemCount() = items.size
+    override fun getItemId(position: Int) = position.toLong()
+    override fun getItemViewType(position: Int) = position
+
+    override fun onBindViewHolder(p0: Holder, p1: Int) {
+        val item = items[p1]
+        p0.bind(item)
+    }
+
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+        fun bind(item: ItemUlasan) {
+            itemView.apply {
+                textViewItemLaporan.text = item.isi
+                setOnClickListener { callback.itemClick(item) }
+            }
+
+
+        }
+
+
+    }
+
+    interface ItemAdapterCallback {
+        fun itemClick(item: ItemUlasan)
+
+    }
 }
