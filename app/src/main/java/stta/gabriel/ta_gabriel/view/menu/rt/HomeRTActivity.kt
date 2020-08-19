6fd98c628e0e5e1aa19f2f2e32d80a660b0a0d2e
adesamp <ade@sampingan.co.id>
2020-08-20 02:04:52 +0700

    setup logout rt

--- app/src/main/java/stta/gabriel/ta_gabriel/view/menu/rt/HomeRTActivity.kt
+++ app/src/main/java/stta/gabriel/ta_gabriel/view/menu/rt/HomeRTActivity.kt
@@ -10,7 +10,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_laporan.*
 import stta.gabriel.ta_gabriel.R
 import stta.gabriel.ta_gabriel.base.BaseActivity
-import stta.gabriel.ta_gabriel.util.SELECTED_MENU
+import stta.gabriel.ta_gabriel.util.*
+import stta.gabriel.ta_gabriel.view.auth.LoginActivity
 import stta.gabriel.ta_gabriel.view.menu.rt.tambahlaporan.TambahLaporanFragment
 import stta.gabriel.ta_gabriel.view.menu.rt.ulasan.UlasanRTFragment
 
@@ -18,6 +19,7 @@ class HomeRTActivity : BaseActivity() {
 
     private var lastTab = 0
     var fragment: Fragment? = null
+    private lateinit var preferences: SharedPrefs
 
     private val mOnNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
@@ -42,6 +44,7 @@ class HomeRTActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_rt)
         setTitle("Menu RT")
+        preferences = SharedPrefs(this)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         savedInstanceState?.getInt(SELECTED_MENU) ?: setNavTab()
     }
@@ -55,6 +58,25 @@ class HomeRTActivity : BaseActivity() {
         outState.putInt(SELECTED_MENU, navigation.selectedItemId)
     }
 
+    override fun onBackPressed() {
+        showDialog(
+            cancelable = false,
+            cancelableTouchOutside = true
+        ) {
+            setTitle("Apakah anda ingin keluar?")
+            positiveButton("Logout") {
+                preferences.saveInt(IS_LOGGED_IN, 0)
+                startActivity(Intent(this@HomeRTActivity, LoginActivity::class.java))
+                finishAffinity()
+            }
+            negativeButton("Keluar") {
+                super.onBackPressed()
+            }
+            neutralButton("Batal") {
+
+            }
+        }
+    }
 
     companion object {
         fun start(activity: Activity) {
